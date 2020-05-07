3839b6bccb609930392dd50d76f3281c654b78da
Paweł Trojanowski <lewapoel@gmail.com>
2020-05-07 13:25:52 +0200

    Enter

--- src/components/AboutPage/About.js
+++ src/components/AboutPage/About.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
+import styles from "./About.module.css";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+
 class App extends Component {
   render() {
-    const style = {
-      width: "300px",
-      height: "300px",
-    };
     return (
-      <div className="container">
+      <div id={styles.styleMap}>
         Hello World
         <Map
           google={this.props.google}
@@ -16,7 +14,6 @@ class App extends Component {
             lat: 35.5496939,
             lng: -120.7060049,
           }}
-          style={style}
         >
           <Marker />
         </Map>
